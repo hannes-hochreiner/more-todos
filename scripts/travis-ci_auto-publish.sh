0ed92b114b7c10a72768a1b897e36d39dd3fcbc1
Hannes Hochreiner <hannes@hochreiner.net>
2016-08-28 21:56:32 +0200

    fixed publishing script

--- scripts/travis-ci_auto-publish.sh
+++ scripts/travis-ci_auto-publish.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_REPO_SLUG" == "hannes-hochreiner/more-todos" ] && [ "$TRAVIS_PULL_
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hannes-hochreiner/more-todos gh-pages > /dev/null
   cd gh-pages
   git rm -rf .
-  cp -rf $HOME/export/bld .
+  cp -rf $HOME/export/bld/* .
   git add -f .
   git commit -m "Lastest version on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
