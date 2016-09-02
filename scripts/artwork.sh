dbe55e5e532896a187f1ebfc0e1e9d4bca8f2087
Hannes Hochreiner <hannes@hochreiner.net>
2016-09-02 07:37:10 +0200

    rearranged build process to accommodate travis

---
+++ scripts/artwork.sh
@@ -0,0 +1,3 @@
+inkscape --without-gui --export-png=src/icon-2x.png --export-width=96 --export-height=96 art/icon.svg
+inkscape --without-gui --export-png=src/icon-3x.png --export-width=144 --export-height=144 art/icon.svg
+inkscape --without-gui --export-png=src/icon-4x.png --export-width=192 --export-height=192 art/icon.svg
