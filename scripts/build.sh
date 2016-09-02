114b1eb83e4bf09b78c4187d8f3ab860db85a2b8
Hannes Hochreiner <hannes@hochreiner.net>
2016-09-02 07:49:39 +0200

    removed short hand notation from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,7 @@ riot --type babel src/todolist.tag bld/todolist.js
 riot --type babel src/todo.tag bld/todo.js
 cp src/index.html bld
 cp src/manifest.json bld
-cp src/icon-{2,3,4}x.png bld
+cp src/icon-2x.png bld
+cp src/icon-3x.png bld
+cp src/icon-4x.png bld
 cp node_modules/riot/riot.min.js bld
