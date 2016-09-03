bb8adf124eff10edcc7e5448cdc1760ee82074ae
travis-ci <travis@travis-ci.org>
2016-09-03 07:10:11 +0000

    Lastest version on successful travis build 34 auto-pushed to gh-pages

--- todo.js
+++ todo.js
@@ -1,9 +1,5 @@
-riot.tag2('mr-todo', '<input type="checkbox" value="{todo.done}" onclick="{toggle}">{todo.text}</input>', '', '', function(opts) {
+riot.tag2('mr-todo', '<span>{todo.text}</span>', '', '', function(opts) {
 "use strict";
 
 this.todo = opts.todo;
-
-this.toggle = function () {
-  console.log("toggle");
-};
 });
--- todolist.js
+++ todolist.js
@@ -1,10 +1,13 @@
-riot.tag2('mr-todolist', '<form onsubmit="{createNewTodo}"> <input type="text" name="newTodoText"></input> <button>submit</button> </form> <ul> <li each="{todo in todos}"><mr-todo todo="{todo}"></mr-todo></li> </ul>', '', '', function(opts) {
+riot.tag2('mr-todolist', '<form onsubmit="{createNewTodo}"> <div class="mdl-textfield mdl-js-textfield"> <input class="mdl-textfield__input" type="text" id="newTodoText"> <label class="mdl-textfield__label" for="newTodoText">new todo...</label> </div> </form> <ul class="demo-list-control mdl-list"> <li each="{todo in todos}" class="mdl-list__item"> <span class="mdl-list__item-primary-content"> <mr-todo todo="{todo}"></mr-todo> </span> <span class="mdl-list__item-secondary-action"> <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-{todo._id}"> <input type="checkbox" id="list-checkbox-{todo._id}" class="mdl-checkbox__input"> </label> </span> </li> </ul>', '', '', function(opts) {
 "use strict";
 
 this.todos = [];
 
 this.createNewTodo = function () {
-  this.todos.push({ "text": this.newTodoText.value });
-  this.newTodoText.value = "";
+  this.todos.push({
+    "_id": this.todos.length,
+    "text": this.newTodoText.value
+  });
+  this.newTodoText.value = null;
 };
 });
