f45ec32d561d95f37d2d5c469e55773af0e58909
travis-ci <travis@travis-ci.org>
2016-09-03 11:08:19 +0000

    Lastest version on successful travis build 36 auto-pushed to gh-pages

--- todo.js
+++ todo.js
@@ -1,5 +1,17 @@
-riot.tag2('mr-todo', '<span>{todo.text}</span>', '', '', function(opts) {
+riot.tag2('mr-todo', '<span class="mdl-list__item-primary-content"> {todo.text} </span> <span class="mdl-list__item-secondary-action"> <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"> <input type="checkbox" class="mdl-checkbox__input"></input> </label> </span>', '', 'class="mdl-list__item"', function(opts) {
 "use strict";
 
 this.todo = opts.todo;
+
+this.on("updated", function () {
+  this.upgradeRecursively(this.root);
+});
+
+this.upgradeRecursively = function (elem) {
+  componentHandler.upgradeElement(elem);
+
+  for (var childCntr = 0; childCntr < elem.children.length; childCntr++) {
+    this.upgradeRecursively(elem.children[childCntr]);
+  }
+};
 });
--- todolist.js
+++ todolist.js
@@ -1,4 +1,4 @@
-riot.tag2('mr-todolist', '<form onsubmit="{createNewTodo}"> <div class="mdl-textfield mdl-js-textfield"> <input class="mdl-textfield__input" type="text" id="newTodoText"> <label class="mdl-textfield__label" for="newTodoText">new todo...</label> </div> </form> <ul class="demo-list-control mdl-list"> <li each="{todo in todos}" class="mdl-list__item"> <span class="mdl-list__item-primary-content"> <mr-todo todo="{todo}"></mr-todo> </span> <span class="mdl-list__item-secondary-action"> <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-{todo._id}"> <input type="checkbox" id="list-checkbox-{todo._id}" class="mdl-checkbox__input"> </label> </span> </li> </ul>', '', '', function(opts) {
+riot.tag2('mr-todolist', '<form onsubmit="{createNewTodo}"> <div class="mdl-textfield mdl-js-textfield"> <input class="mdl-textfield__input" type="text" id="newTodoText"> <label class="mdl-textfield__label" for="newTodoText">new todo...</label> </div> </form> <div class="mdl-list"> <mr-todo each="{todo in todos}" todo="{todo}"></mr-todo> </div>', '', '', function(opts) {
 "use strict";
 
 this.todos = [];
