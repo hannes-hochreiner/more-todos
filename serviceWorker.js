e94e31e216c339ebcc162996342b4c0b08c5ec05
travis-ci <travis@travis-ci.org>
2016-09-12 05:25:07 +0000

    Lastest version on successful travis build 91 auto-pushed to gh-pages

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-var currentCache = "more-todos-2016-09-11T21:07:19+0000";
+var currentCache = "more-todos-2016-09-12T05:25:00+0000";
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(currentCache).then(function(cache) {
